8a217afac6bb024a7524e20b8bb838693ecb7953
Tyler Compton <xaviosx@gmail.com>
2025-04-29 17:15:10 -0700

    Use open source Nvidia GPU drivers
    Apparently they're better for newer cards

--- systems/raise/modules/gpus.nix
+++ systems/raise/modules/gpus.nix
@@ -14,7 +14,7 @@
     modesetting.enable = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-    open = false;
+    open = true;
     prime = {
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
