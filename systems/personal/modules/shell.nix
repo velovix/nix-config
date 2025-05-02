4bd87f748273b7330ed1d40ea1e6d0ecfe14fcd3
Tyler Compton <xaviosx@gmail.com>
2025-05-02 11:32:45 -0700

    Fix zsh Ctrl+Arrow Key shortcut

--- systems/personal/modules/shell.nix
+++ systems/personal/modules/shell.nix
@@ -1,11 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, options, ... }: {
   users.defaultUserShell = pkgs.zsh;
+
+  # Prevents the new user dialog from constantly appearing
+  system.userActivationScripts.zshrc = "touch .zshrc";
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
+    # Fixes Ctrl+Left and Ctrl+Right
+    promptInit = options.programs.zsh.promptInit.default + ''
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
+    '';
   };
 }
 
