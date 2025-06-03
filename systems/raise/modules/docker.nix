421205085ec485f38ef46bd31f47311bf71589dc
Tyler Compton <xaviosx@gmail.com>
2025-06-03 12:04:08 -0700

    Move away from legacy Nvidia runtime

--- systems/raise/modules/docker.nix
+++ systems/raise/modules/docker.nix
@@ -1,24 +1,20 @@
-{ config, pkgs, libs, ... }:
+{ config, pkgs, lib, libs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     nvidia-container-toolkit
-    # This provides access to the more legacy Nvidia Docker stuff, like the
-    # nvidia runtime
-    nvidia-docker
   ];
-
-  hardware.graphics = {
-    # This is necessary for virtualisation.docker.enableNvidia
-    enable32Bit = true;
-  };
 
   virtualisation.docker = {
     enable = true;
-    # This may also help with legacy Nvidia Docker
-    enableNvidia = true;
     # I don't need this feature and it causes NixOS to hang on shutdown
     liveRestore = false;
+    daemon.settings = {
+      features = {
+        # For nvidia-container-toolkit
+        cdi = true;
+      };
+    };
   };
   users.extraGroups.docker.members = [ "tyler" ];
 
