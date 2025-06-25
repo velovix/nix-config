55c901c484e89d40ecbc69b3bfc9db39b33aa9a2
Tyler Compton <tyler@raiserobotics.ai>
2025-06-25 10:46:31 -0700

    Add workaround for VBox with kernel 6.12

--- systems/raise/modules/virtualbox.nix
+++ systems/raise/modules/virtualbox.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Fixes an issue with KVM/VirtualBox compatibility introduced in kernel 6.12
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
+
   virtualisation.virtualbox.host = {
     enable = true;
     enableExtensionPack = true;
